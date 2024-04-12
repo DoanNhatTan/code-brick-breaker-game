8a0f77d4d2ffb585c9cbc417d3e2c6254a8b9a9a
nhattan2k3gl <nhattan2k3gl@gmail.com>
2024-04-12 14:41:48 +0700

    #4 move bar

--- breakout.js
+++ breakout.js
@@ -5,12 +5,14 @@ let boardHeight =500;
 // player
 let playerWidth = 80;
 let playerHeight =10;
+let playerVelocityX = 10
 
 let player = {
     x : boardWidth/2 - playerWidth/2,
     y: boardHeight - playerHeight - 5,
     width : playerWidth,
-    height: playerHeight
+    height: playerHeight,
+    velocityX: playerVelocityX
 }
 
 window.onload = function() {
@@ -19,9 +21,25 @@ window.onload = function() {
     board.width = boardWidth;
     context = board.getContext("2d"); // used for drawing on the board
 
-    //draw initial player
+    requestAnimationFrame(update); // draw the frame more smoothly
+    document.addEventListener("keydown", movePlayer); // the keydown are on the keyboard (a,w,s,d)
+}
+
+function update(){
+    requestAnimationFrame(update); // Đệ quy ?
+    context.clearRect(0, 0, board.width, board.height);
+
+    //player
     context.fillStyle = "lightgreen";
     context.fillRect(player.x, player.y, player.width, player.height) 
 }
 
+function movePlayer(e){
+    if(e.code == "ArrowLeft"){
+        player.x -= player.velocityX;
+    }
+    else if (e.code == "ArrowRight"){
+        player.x += player.velocityX;
+    }
+}
 
